33e448b399035a04ad0563f4b53c38d18adf8518
Eduardo Villegas <ludentico@yahoo.com>
2023-04-01 10:39:57 -0600

    Fixed some grammar

--- lang/es/settings.php
+++ lang/es/settings.php
@@ -5,7 +5,7 @@
  * spanish language file
  */
 $lang['addpage_exclude']  = "excluir directorio(namespace) separados por punto y coma (;)";
-$lang['addpage_showroot'] = "Mostrar directorio(namespace) raiz";
-$lang['addpage_hide']     = "cuando se usa la sintaxis {{NEWPAGE>[dir]}}: se oculta el selector de directorio (desmarcado: muestra solo sub-directorios (sub-namespaces))";
-$lang['addpage_hideACL']  = "si esta desmarcado, muesta un mensaje de error de privilegios. Si el usuario no tiene permiso de crear una pagina, simplemente se oculta el codigo {{NEWPAGE}}"
+$lang['addpage_showroot'] = "Mostrar directorio(namespace) raíz";
+$lang['addpage_hide']     = "Cuando se usa la sintaxis {{NEWPAGE>[dir]}}: se oculta el selector de directorio (desmarcado: muestra solo sub-directorios (sub-namespaces))";
+$lang['addpage_hideACL']  = "Si está desmarcado, muestra un mensaje de error de privilegios. Si el usuario no tiene permiso de crear una página, simplemente se oculta el código {{NEWPAGE}}"
 ?>
